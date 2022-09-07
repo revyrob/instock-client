5c8807a2d5ea9d3b6e47d84b6005b986f46b2c6b
Nikhil <nikhilshetye24@gmail.com>
2022-09-06 17:28:37 -0700

    adeed styles for header to be repsonsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,17 @@
-import logo from '../../assets/logo/InStock-Logo_2x.png'
+import logo from '../../assets/logo/InStock-Logo_1x.png'
 import './Header.scss'
 export default function Header(){
     return(
-        <header>
-            <navbar className="nav">
-                <a href="" className="nav__logo-link">
-                    <img src={logo} alt="logo" className="nav__logo-img" />
-                </a>
-                <a href="" className="nav__link-item nav__link-item--active">Warehouses</a>
-                <a href="" className="nav__link-item">Inventory</a>
-            </navbar>
-        </header>
+        <div>
+            <header className='nav__header'>
+                <navbar className="nav">
+                    <a href="" className="nav__logo-link">
+                        <img src={logo} alt="logo" className="nav__logo-img" />
+                    </a>
+                    <a href="" className="nav__link-warehouse nav__link-item--active">Warehouses</a>
+                    <a href="" className="nav__link-inventory">Inventory</a>
+                </navbar>
+            </header>
+        </div>  
     )
 }
