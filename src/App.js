03b60d352d0535dec21e1db0324c042f698442c5
Andre Foulem <timmycasual@icloud.com>
2022-09-08 17:20:26 -0700

    change the class names in inventorydetails to avoid class collision with warehousedetails

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Header></Header>
+
       <Routes>
         <Route path="/warehouses" element={<Warehouses />} />
         <Route
@@ -19,13 +20,7 @@ function App() {
           path="/warehouse/new"
           element={<EditWarehouse></EditWarehouse>}
         />
-        {/* <Route path="/Warehouse/:id" element={<WareHouses />} />
-        <Route path="/Warehouse/add" element={<WareHouses />} />
-        
-        <Route path="/Inventory" element={<Inventory />} />
-        <Route path="/Inventory/:id" element={<Inventory />} />
-        <Route path="/Inventory/add" element={<Inventory />} />
-        <Route path="/Inventory/:id/edit" element={<Inventory />} /> */}
+
         <Route path="/warehousedetails" element={<WarehouseDetails />} />
         <Route path="/inventorydetails" element={<InventoryDetails />} />
       </Routes>
--- src/pages/InventoryDetails/InventoryDetails.js
+++ src/pages/InventoryDetails/InventoryDetails.js
@@ -19,112 +19,117 @@ export default function WarehouseDetails() {
     });
   }, []);
   return (
-    <section className="warehouseDetails">
-      <div className="pageHeader">
-        <div className="pageHeader__title">
-          <p className="pageHeader__warehouse-name">Inventory</p>
+    <section className="inventoryDetails">
+      <div className="in-pageHeader">
+        <div className="in-pageHeader__title">
+          <p className="in-pageHeader__warehouse-name">Inventory</p>
         </div>
-        <div className="pageHeader__flexbox">
+        <div className="in-pageHeader__flexbox">
           <SearchBar />
-          <div className="pageHeader__addItemBtn">
-            <span className="pageHeader__addItemBtnText"> + Add New Item</span>
+          <div className="in-pageHeader__addItemBtn">
+            <span className="in-pageHeader__addItemBtnText">
+              {' '}
+              + Add New Item
+            </span>
           </div>
         </div>
       </div>
 
-      <div className="stillBox">
-        <div className="stillBox__box1">
-          <p className="stillBox__labelTable">INVENTORY ITEM</p>
+      <div className="in-stillBox">
+        <div className="in-stillBox__box1">
+          <p className="in-stillBox__labelTable">INVENTORY ITEM</p>
           <img
-            className="stillBox__arrowSort"
+            className="in-stillBox__arrowSort"
             src={arrowSort}
             alt="icon chevron"
           />
         </div>
-        <div className="stillBox__box2">
+        <div className="in-stillBox__box2">
           {' '}
-          <p className="stillBox__labelTable">CATEGORY</p>
+          <p className="in-stillBox__labelTable">CATEGORY</p>
           <img
-            className="stillBox__arrowSort"
+            className="in-stillBox__arrowSort"
             src={arrowSort}
             alt="icon chevron"
           />
         </div>
-        <div className="stillBox__box3">
+        <div className="in-stillBox__box3">
           {' '}
-          <p className="stillBox__labelTable">STATUS</p>
+          <p className="in-stillBox__labelTable">STATUS</p>
           <img
-            className="stillBox__arrowSort"
+            className="in-stillBox__arrowSort"
             src={arrowSort}
             alt="icon chevron"
           />
         </div>
-        <div className="stillBox__box4">
+        <div className="in-stillBox__box4">
           {' '}
-          <p className="stillBox__labelTable">QTY</p>
+          <p className="in-stillBox__labelTable">QTY</p>
           <img
-            className="stillBox__arrowSort"
+            className="in-stillBox__arrowSort"
             src={arrowSort}
             alt="icon chevron"
           />
         </div>
-        <div className="stillBox__box5">
+        <div className="in-stillBox__box5">
           <p>WAREHOUSE</p>
           <img
-            className="stillBox__arrowSort"
+            className="in-stillBox__arrowSort"
             src={arrowSort}
             alt="icon chevron"
           />
         </div>
 
-        <div className="stillBox__box7">ACTIONS</div>
+        <div className="in-stillBox__box7">ACTIONS</div>
       </div>
 
       {inventoriesArr &&
         inventoriesArr.map((inventory) => (
-          <div className="magicBox" key={uuid()}>
-            <div className="magicBox__box1">
+          <div className="in-magicBox" key={uuid()}>
+            <div className="in-magicBox__box1">
               {' '}
-              <label className="magicBox__labelMobile">INVENTORY ITEM</label>
+              <label className="in-magicBox__labelMobile">INVENTORY ITEM</label>
               <div className="flexbox">
-                <p className="magicBox__labelItem">{inventory.itemName}</p>
+                <p className="in-magicBox__labelItem">{inventory.itemName}</p>
                 <img
-                  className="magicBox__chevron"
+                  className="in-magicBox__chevron"
                   src={chevron}
                   alt="icon chevron"
                 />
               </div>
             </div>
-            <div className="magicBox__box2">
-              <label className="magicBox__labelMobile">Category</label>
-              <span className="magicBox__categoryValue">
+            <div className="in-magicBox__box2">
+              <label className="in-magicBox__labelMobile">Category</label>
+              <span className="in-magicBox__categoryValue">
                 {inventory.category}
               </span>
             </div>
-            <div className="magicBox__box3">
-              <label className="magicBox__labelMobile">STATUS</label>
+            <div className="in-magicBox__box3">
+              <label className="in-magicBox__labelMobile">STATUS</label>
 
               <span
                 className={`${
                   inventory.status === 'In Stock'
-                    ? 'magicBox__inStock'
-                    : 'magicBox__outOfStock'
+                    ? 'in-magicBox__inStock'
+                    : 'in-magicBox__outOfStock'
                 }`}
               >
                 {inventory.status}
               </span>
             </div>
-            <div className="magicBox__box4">
-              <label className="magicBox__labelMobile">QTY</label>{' '}
-              <span className="magicBox__qtyValue">{inventory.quantity}</span>
+            <div className="in-magicBox__box4">
+              <label className="in-magicBox__labelMobile">QTY</label>{' '}
+              <span className="in-magicBox__qtyValue">
+                {inventory.quantity}
+              </span>
             </div>
-            <div className="magicBox__box5">
-              <label className="magicBox__labelMobile">WAREHOUSE</label>{' '}
-              <span class="magicBox__warehouseNameValue">
+            <div className="in-magicBox__box5">
+              <label className="in-magicBox__labelMobile">WAREHOUSE</label>{' '}
+              <span class="in-magicBox__warehouseNameValue">
                 {inventory.warehouseName}
               </span>
             </div>
-            <div className="magicBox__box6">
+            <div className="in-magicBox__box6">
               <img src={delteCan} alt="delete icon" />
               <img src={editPen} alt="edit icon" />
             </div>
