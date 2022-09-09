380ae946f349d2bb942a20c7e7b62138b8d54ab0
Andre Foulem <timmycasual@icloud.com>
2022-09-08 21:34:41 -0700

    Implement HTML for InventoryItem. Prepared for hydration

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Warehouses from './pages/Warehouses/Warehouses';
 import WarehouseDetails from './pages/WarehouseDetails/WarehouseDetails';
 import InventoryDetails from './pages/InventoryDetails/InventoryDetails';
+import InventoryItem from './pages/InventoryItem/InventoryItem';
 import EditWarehouse from './pages/EditWarehouse/EditWarehouse';
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <Route path="/warehouses/new" element={<EditWarehouse />} />
         <Route path="/warehouses/:id/edit" element={<EditWarehouse />} />
 
-        <Route path="/inventory/:id" element={<Inventory />} />
+        <Route path="/inventory/:id" element={<InventoryItem />} />
         {/* <Route path="/inventory" element={<Inventory />} />
         <Route path="/inventory/new" element={<Inventory />} />
         <Route path="/inventory/:id/edit" element={<Inventory />} /> 
